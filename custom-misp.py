a7768bdbe02580324750c92b0728b959d07ff6b2
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2025-07-30 09:19:48 +0530

    Update custom-misp.py

--- custom-misp.py
+++ custom-misp.py
@@ -21,7 +21,7 @@ LOG_IOCS_FILE = "/var/ossec/logs/misp-iocs.log"
 LOG_ALERTS_FILE = "/var/ossec/logs/misp-alerts.log"
 LOG_INTEGRATION_FILE = "/var/ossec/logs/misp-integration.log"
 MISP_BASE_URL = "https://cti.codesec.in/attributes/restSearch/"
-MISP_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+MISP_API_KEY = "wKanbzkcbKh1n4OzttPaw3d1qxv0IVWd5H3Ug3DO"
 MISP_SSL_VERIFY = False
 SOCKET_PATH = f"{os.path.dirname(os.path.dirname(os.path.realpath(__file__)))}" + "/queue/sockets/queue"
 
